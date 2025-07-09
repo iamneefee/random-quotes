f5e124aa1fcadfb65b1150160a84c89a8cc2c454
neefee <contact@neefee.ru>
2025-07-09 21:22:39 +0300

    feat: added methods to increment likes, views and dislikes

--- quotes/models.py
+++ quotes/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+from django.db.models import F
 from django.core.validators import MinValueValidator, MaxValueValidator
+
+MIN_WEIGHT, MAX_WEIGHT = 0, 10
 
 
 class Source(models.Model):
@@ -21,7 +24,7 @@ class Quote(models.Model):
     )
     weight = models.PositiveIntegerField(
         default=5,
-        validators=[MinValueValidator(0), MaxValueValidator(10)],
+        validators=[MinValueValidator(MIN_WEIGHT), MaxValueValidator(MAX_WEIGHT)],
     )
     views = models.PositiveIntegerField(default=0)
     likes = models.PositiveIntegerField(default=0)
@@ -32,3 +35,18 @@ class Quote(models.Model):
     def __str__(self):
         text = f"{self.text[:25]}..." if len(self.text) > 25 else self.text
         return f"{text} (weight: {self.weight}, likes: {self.likes}, dislikes: {self.dislikes})"
+
+    def add_view(self):
+        self.views = F('views') + 1
+        self.save(update_fields=['views'])
+        self.refresh_from_db()
+
+    def add_like(self):
+        self.likes = F('likes') + 1
+        self.save(update_fields=['likes'])
+        self.refresh_from_db()
+
+    def add_dislike(self):
+        self.dislikes = F('dislikes') + 1
+        self.save(update_fields=['dislikes'])
+        self.refresh_from_db()
