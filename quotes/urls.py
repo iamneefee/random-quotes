d15f50b5ac109e256d8a6c9bd71b2aba1358d48e
neefee <contact@neefee.ru>
2025-07-10 18:46:57 +0300

    feat: implemented like\dislike functionality to the main page

--- quotes/urls.py
+++ quotes/urls.py
@@ -5,4 +5,5 @@ app_name = 'quotes'
 
 urlpatterns = [
     path('', views.random_quote, name='random-quote'),
+    path('rate/', views.rate_quote, name='rate-quote'),
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -1,4 +1,5 @@
 import random
+
 from django.shortcuts import render
 from .models import Quote
 
@@ -11,12 +12,27 @@ def get_random_quote():
 
     weights = [quote.weight for quote in quotes]
     quote = random.choices(quotes, weights=weights, k=1)[0]
-    quote.add_view()
 
     return quote
 
 
 def random_quote(request):
     quote = get_random_quote()
+    quote.add_view()
 
     return render(request, 'quotes/pages/random_quote.html', {'quote': quote})
+
+
+def rate_quote(request):
+    if request.method == 'POST':
+        quote_id = request.POST.get('quote_id')
+        action = request.POST.get('action')
+        quote = Quote.objects.get(id=quote_id)
+
+        if action == 'like':
+            quote.add_like()
+
+        elif action == 'dislike':
+            quote.add_dislike()
+
+        return render(request, 'quotes/components/quote_card.html', {'quote': quote})
