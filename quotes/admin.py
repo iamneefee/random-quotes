26658e4a284de58ee3421e96a94c7185e546f76f
neefee <contact@neefee.ru>
2025-07-13 14:27:21 +0300

    feat: added admin panel for models

--- quotes/admin.py
+++ quotes/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Source, Quote
 
-# Register your models here.
+
+@admin.register(Quote)
+class QuoteAdmin(admin.ModelAdmin):
+    list_display = ('short_text', 'source', 'weight', 'views', 'likes', 'dislikes')
+    list_editable = ('weight',)
+    search_fields = ('text', 'source__name')
+
+    @staticmethod
+    def short_text(obj):
+        return obj.text[:50] + '...' if len(obj.text) > 50 else obj.text
+
+
+@admin.register(Source)
+class SourceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author')
+    search_fields = ('name', 'author')
