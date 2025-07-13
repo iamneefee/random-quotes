f58b729fca1eb3474132095e7a1f0e40aa450b39
neefee <contact@neefee.ru>
2025-07-13 14:28:25 +0300

    feat: added add quote and source pages, added alerts to base template

--- quotes/urls.py
+++ quotes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views.random_quote import random_quote_view, rate_quote_view
 from .views.dashboard import dashboard_view
+from .views.add_entry import add_quote_view, add_source_view
 
 app_name = 'quotes'
 
@@ -8,4 +9,6 @@ urlpatterns = [
     path('', random_quote_view, name='random-quote'),
     path('rate/', rate_quote_view, name='rate-quote'),
     path('dashboard/', dashboard_view, name='dashboard'),
+    path('add/source/', add_source_view, name='add-source'),
+    path('add/quote/', add_quote_view, name='add-quote'),
 ]
